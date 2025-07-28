46a4b3953e63f1387bee4d3aab86c8139e6f3f3d
Rafael Moncores <rafamoncores@gmail.com>
2025-07-28 14:30:35 -0300

    feat: add devise and styling

--- Gemfile
+++ Gemfile
@@ -5,6 +5,13 @@ ruby "3.1.4"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.5", ">= 7.1.5.1"
 
+gem "devise"
+# gem "twitter-bootstrap-rails"
+gem "bootstrap"
+gem "sassc-rails"
+gem "devise-bootstrap-views"
+gem "jquery-rails"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'danger'  if type == 'alert'
+      text = content_tag(:div, message.html_safe, class: "alert alert-#{type} alert-dismissible fade show", role: "alert") do
+               content_tag(:button, type: "button", class: "btn-close", "data-bs-dismiss" => "alert", "aria-label" => "Close") do
+                 content_tag(:span, "&times;".html_safe, "aria-hidden" => "true")
+               end + message.html_safe
+             end
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
